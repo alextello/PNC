82da1b47cde412dd8d338b3896fe51c032bd82f3
alextello <alextello1@hotmail.com>
2018-04-20 21:39:04 -0600

    menu

---
+++ resources/views/admin/partials/nav.blade.php
@@ -0,0 +1,17 @@
+<ul class="sidebar-menu" data-widget="tree">
+        <li class="header">Navegación</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="fa fa-home"></i> <span>Inicio</span></a></li>
+        {{-- <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li> --}}
+        <li class="treeview">
+          <a href="#"><i class="fa fa-clipboard"></i> <span>Reportes</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-eye"></i>Ver reportes</a></li>
+            <li><a href="#"><i class="fa fa-pencil"></i>Crear reporte</a></li>
+          </ul>
+        </li>
+ </ul>
